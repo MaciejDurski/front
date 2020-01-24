ef9ff0efbf24168d474363cc53fa05ce6d0d692d
MaciejDurski <maciej-durski1@wp.pl>
2020-01-24 17:05:32 +0100

    wow

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 var image = document.getElementsByClassName('offer-img');
 new simpleParallax(image,
     {
-        delay:1.5,
-        scale:1.5
+        delay:2.8,
+        scale:1.7
     });
+
+
+    staticBtn = document.getElementById("staticBtn");
+
+    var offer = document.getElementById("offer");
+
+    window.onscroll = function() {scrollFunction()};
+
+    
+    function scrollFunction(){
+        if (document.body.scrollTop>20||document.documentElement.scrollTop>20)
+        {
+            staticBtn.style.transform='rotate(180deg)';
+        }else
+        {
+            staticBtn.style.transform='rotate(360deg)';
+        }
+    }
+
+    function topFunction()
+    {
+        if (document.body.scrollTop>20||document.documentElement.scrollTop>20)
+        {
+            document.body.scrollTop=0;
+            document.documentElement.scrollTop =0;
+        }else
+        {
+            offer.scrollIntoView();
+            
+        }
+
+
+    }
+
+
+  
+
+  
